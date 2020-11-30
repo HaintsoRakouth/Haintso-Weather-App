920d464558f20ca0cd76560f7e06100daf3cf901
HaintsoRakouth <haintsor@gmail.com>
2020-11-29 19:43:32 -0500

    Fixed errors in HTML, JS and CSS
    HTML included wrong link to JS file
    JS - lowercase Form and changed city displayed to Palaiseau

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 function formatDate(timestamp) {
-  let date = new Date(timestamp);
+    let date = new Date(timestamp);
   
-  let days = [
+
+    let days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -9,12 +10,14 @@ function formatDate(timestamp) {
     "Thursday",
     "Friday",
     "Saturday"
-  ];
+    ];
 
-  let day = days[date.getDay()];
-  return `${day} ${formatHours(timestamp)}`;
+    let day = days[date.getDay()];
+    return `${day} ${formatHours(timestamp)}`;
+
 }
-  function formatHours(timestamp) {
+
+function formatHours(timestamp) {
   let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours < 10) {
@@ -27,9 +30,8 @@ function formatDate(timestamp) {
     }
       return`${hours}:${minutes}`;
 }
-
 function displayTemperature(response){
-  console.log(response.data);
+    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let conditionElement = document.querySelector("#condition");
@@ -50,7 +52,6 @@ iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data
 );
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
 
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
@@ -76,34 +77,40 @@ forecastElement.innerHTML += `
   }
 
 
-function searchCity(city) {
-  let apiKey = "3b86af7fc308a37a703e9fe00272cd4f";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
 
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+function searchCity(city){
+let apiKey = "3b86af7fc308a37a703e9fe00272cd4f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+axios.get(apiUrl).then(displayTemperature);
+
+apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 axios.get(apiUrl).then(displayForecast);
+
 }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  searchCity(cityInputElement.value);
-  console.log(cityInputElement);
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    searchCity(cityInputElement.value);
+    console.log(cityInputElement);
 }
 
-function searchLocation(position) {
+function searchLocation(position){
 let apiKey = "3b86af7fc308a37a703e9fe00272cd4f";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
 axios.get(apiUrl).then(displayTemperature);
 
 apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
 axios.get(apiUrl).then(displayForecast);
+
 }
 
-function getCurrentLocation(event) {
+function getCurrentLocation(event){
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
+  
+  
 }
 
 function displayCelsiusTemperature(event) {
@@ -114,6 +121,7 @@ fahrenheitLink.classList.remove("active");
 celsiusLink.classList.add("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
+
 }
 
 function displayFahrenheitTemperature(event) {
@@ -126,18 +134,16 @@ function displayFahrenheitTemperature(event) {
 
 let fahrenheitTemperature = null;
 
-//Submit via search bar
-let Form = document.querySelector("#search-form");
-Form.addEventListener("submit", handleSubmit);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
-//Displaying Celsius and Fahrenheit
+
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-//Display Current Location and Default Location Palaiseau
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
